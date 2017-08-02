af86aae1bd13d373e019ec6b436d683dd812263b
Brandon Walden <brandonshanewalden@gmail.com>
2017-08-02 19:40:56 -0400

    added form

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator')
 const moviesSchema = require('./models/schema.js');
 const MongoClient = require('mongodb').MongoClient;
 let url = 'mongodb://127.0.0.1:27017/moviesdb';
@@ -13,21 +14,12 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/moviesdb');
 
 app.use(express.static('public'))
+app.use(expressValidator())
 
 app.engine('mustache', mustacheExpress());
 
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
-
-const Movie = mongoose.model('Movie', moviesSchema);
-let movie = new Movie({title: "Stay"});
-movie.genre.push("Drama");
-
-movie.save().then(function(){
-console.log('movie saved');
-}).catch(function(){
-  console.log("Mongo couldn\'t save movie");
-});
 
 let findAll = function(db, callback) {
   let collection = db.collection('movies');
@@ -45,6 +37,33 @@ app.get('/', function(request, response) {
       });
     });
   });
+});
+
+app.post('/', function(request, response) {
+
+
+request.assert(moviesSchema);
+const Movie = mongoose.model('Movie', moviesSchema);
+let movie = new Movie({title: "Stay"});
+movie.genre.push("Drama");
+
+movie.save().then(function(){
+console.log('movie saved');
+}).catch(function(){
+  console.log("Mongo couldn\'t save movie");
+
+  request.getValidationResult().then(function(results) {
+  if (results.isEmpty()) {
+    response.render('answers', {
+      answers: request.body
+    });
+  } else {
+    response.render('form', {
+      errors: results.array()
+    });
+  }
+});
+});
 });
 
 app.listen(3000, function() {
